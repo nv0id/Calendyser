540a97827ff6771e2862236dc83c735ebb5ebd9d
nv0id <57645148+nv0id@users.noreply.github.com>
2020-01-28 09:10:27 +0000

    Code

--- Python/sqliser.py
+++ Python/sqliser.py
@@ -12,27 +12,27 @@ def sqlise(path):
     '''
     calendar = calfinder.calfinder(path)
 
-    conn = sqlite3.connect('/Users/nvoidmac/Documents/GitHub/Calendyser/Python/events.db')
+    conn = sqlite3.connect('events.db') # Locates events.db database in same directory, otherwise makes one.
     c = conn.cursor()
 
-    counter = 0
+    query_counter = 0
 
     for calname,path in calendar.calnames.items():
         calname = calfinder.sql_clean(calname) # Cleaniing up the name to prevent SQL injection.
         c.execute("CREATE TABLE "+calname+" (fileid text, name text, location text, start text, finish text,duration text)") # Create a table for each Calendar found
-        counter += 1
+        query_counter += 1
 
-        for eventpath in calfinder.file_ext_search(path,".ics"): # Searches for .ics files
+        for eventpath in calfinder.file_ext_search(path,".ics"): # Searches for .ics files in that .calendar folder
             event = calfinder.event(eventpath) # Creates an instance of event.
             sql_cmd = "INSERT INTO "+calname+" VALUES (%s, %s, %s, %s, %s, %s)" %(event.filename, event.ename, event.elocation, event.ebegin, event.eend, event.duration)
-            counter += 1
+            query_counter += 1
             c.execute(sql_cmd) # Writes event data to DB
 
 
     conn.commit()
     conn.close()
 
-    return ("Made "+str(counter)+" queries!")
+    return ("Made "+str(query_counter)+" queries!")
 
 def main():
     pass
